91f381306a3f5a5f02e733ca87b66306b88de18d
Alissa <99267320+alissarm@users.noreply.github.com>
2023-05-26 12:32:15 -0300

    Add files via upload

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>tela principal</Text>
+      <Link href="/telaA">Ir para A</Link>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: '32',
+    fontWeight: 'bold',
+  },
+});
